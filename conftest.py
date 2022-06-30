1150566dddcbca80081ddfcfd9bde862cf30c35f
bot209 <92056568+bot209@users.noreply.github.com>
2022-06-30 14:35:00 +0300

    add: Добавить тестовое задание
    Ref: https://stepik.org/lesson/237240/step/9?unit=209628

---
+++ conftest.py
@@ -0,0 +1,31 @@
+import pytest
+from webbrowser import Chrome
+from requests import options
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+# Парсим язык сайта
+def pytest_addoption(parser):
+    parser.addoption('--language',
+                    action='store', 
+                    default=None,
+                    help="Choose user language")
+
+# Обьявляем браузер в данной фикстуре и передаем ее как параметр в наш тест
+@pytest.fixture(scope="function")
+def browser(request):
+    lang = request.config.getoption('language')
+    options = Options()
+    options.add_experimental_option('prefs', {'intl.accept_languages': lang})
+    browser = webdriver.Chrome(options=options)
+
+# При запуске браузера на выбраном языке, в логах будет отображаться на каком языке запустился тест
+    if lang == f'{lang}':
+        print(f"\nStart chrome browser for '{lang}' language..")
+    elif lang == f'{lang}':
+        print(f"\nStart chrome browser for '{lang}' language..")
+    else:
+        raise pytest.UsageError('Language should be ru on en')
+    yield browser
+    print(f"\nQuit chrome browser for '{lang}' language..")
+    browser.quit()
